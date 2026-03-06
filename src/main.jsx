964c227fa1c13a21dfbc9b2fa19e4ad7d19f58db
Rajditya <rajaditya2411@gmail.com>
2026-03-07 01:12:57 +0530

    remove supabase integration and revert to local storage version

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-console.log(import.meta.env.VITE_SUPABASE_URL)
 
   ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
